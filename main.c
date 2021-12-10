b04b81637705df0d170c383ffaa2f0ca694e219f
Dennis Hoffmann <dh169187@fh-muenster.de>
2021-12-10 15:48:11 +0100

    Vorgegebene Struktur

--- main.c
+++ main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <malloc.h>
+
+#define M 7
+
+
+static struct node
+{
+    int key;
+    struct node *next;
+};
+
+static struct node *koepfe[M], *ende;
+
+void hashlistinitialize()
+{
+    int i;
+
+    ende = (struct node *) malloc (sizeof *ende);
+    ende->next = ende; ende->key = -1;
+    for (i = 0; i < M; i++)
+    {
+        koepfe[i] = (struct node *) malloc (sizeof *ende);
+        koepfe[i]->next = ende;
+    }
+}
+
+unsigned hash (int i)
+{
+    return i%M;
+}
+
+void hashInsert(int i){
+    //TODO
+}
+
+void printWithStats(){
+    //TODO
+}
+
+
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+
 }
+
+
