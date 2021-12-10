7d7561ca338c64c88c0330c373ee42d551ab5e27
Dennis Hoffmann <dh169187@fh-muenster.de>
2021-12-10 16:45:09 +0100

    Insert und print

--- main.c
+++ main.c
@@ -1,47 +1,81 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <time.h>
 
 #define M 7
+#define MaxKey 100;
 
 
-static struct node
-{
+static struct node{
     int key;
     struct node *next;
 };
 
 static struct node *koepfe[M], *ende;
 
-void hashlistinitialize()
-{
+void hashlistinitialize(){
+    srand(time(NULL));
     int i;
 
     ende = (struct node *) malloc (sizeof *ende);
     ende->next = ende; ende->key = -1;
-    for (i = 0; i < M; i++)
-    {
+    for (i = 0; i < M; i++){
         koepfe[i] = (struct node *) malloc (sizeof *ende);
         koepfe[i]->next = ende;
     }
 }
 
-unsigned hash (int i)
-{
+unsigned hash (int i){
     return i%M;
 }
 
 void hashInsert(int i){
-    //TODO
+    struct node* newNode = malloc(sizeof(struct node));
+    newNode->key = i;
+    newNode->next = ende;
+
+    struct node* tmp = koepfe[hash(i)]->next;
+
+    if (tmp->key == -1){
+        koepfe[hash(i)]->next = newNode;
+        return;
+    }
+
+    while (tmp->next->key != -1){
+        tmp = tmp->next;
+    }
+
+    tmp->next = newNode;
 }
 
-void printWithStats(){
-    //TODO
+void printList(){
+    for(int i = 0; i<M; i++){
+
+        struct node* tmp = koepfe[i]->next;
+        printf("%i: ",i);
+        while (tmp->key != -1){
+            printf("%i",tmp->key);
+            printf(" -> ");
+            tmp = tmp->next;
+        }
+        printf("\n");
+    }
 }
 
+void printStats(){
 
+}
+
+unsigned randomNumber(){
+    return rand()%MaxKey;
+}
 
 int main() {
-
+    hashlistinitialize();
+    for (int i = 0; i<50; i++){
+        hashInsert(randomNumber());
+    }
+    printList();
 }
 
 
